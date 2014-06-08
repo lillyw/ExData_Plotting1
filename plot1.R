3bd6362841902901ba7b3c697013ad774d64c35f
lillyw <lilly.wang@t-mobile.com>
2014-06-08 09:20:50 -0700

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,6 @@
+mydir<-"C:/myR"
+myds<-read.csv(file=paste(mydir, "/household_power_consumption.txt", sep=""), skip=66637, header=FALSE, nrow=2880, sep=";", stringsAsFactors=FALSE) 
+names(myds)<-c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+png(file =paste(mydir, "/plot.png1", sep=""), width = 480, height = 480)
+hist(myds$Global_active_power, xlab="Global Active Power (kilowatts)", main="Global Active Power", col="red")
+dev.off
